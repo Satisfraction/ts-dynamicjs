9aa593fe2da7572c623979477e6d498c986eda46
Satisfraction <mathias.lappe@docc.techstarter.de>
2023-04-27 21:06:59 +0200

    fix plusButton

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,16 @@
-const button = document.getElementById('myButton');
+// const button = document.getElementById('myButton');
 
-button.addEventListener('click', function() {
-	const message = document.createElement('h2');
-	message.textContent = 'Button wurde geklickt!';
-	message.style.color = 'red';
-	document.getElementById('myHeading').appendChild(message);
+// button.addEventListener('click', function() {
+// 	const message = document.createElement('h2');
+// 	message.textContent = 'Button wurde geklickt!';
+// 	message.style.color = 'red';
+// 	document.getElementById('myHeading').appendChild(message);
+// });
+
+const plusButton = document.getElementById('plusButton');
+const counter = document.getElementById('counter');
+
+plusButton.addEventListener('click', function() {
+  const currentValue = parseInt(counter.value);
+  counter.value = currentValue + 1;
 });
