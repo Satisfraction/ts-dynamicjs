1a8534d24f43da3ebfd7638f35adb43c4f697b1f
Edwin Radtke <e>
2023-04-27 13:08:17 +0100

    aufgabe 3 aktualisiert

--- 03/app.js
+++ 03/app.js
@@ -3,10 +3,15 @@ function fetchProduct(id) {
 	  .then((res) => res.json());
   }
   
+  function fetchProduct(id) {
+	return fetch(`https://dummyjson.com/products/${id}`)
+	  .then((res) => res.json());
+  }
+  
   function updateProduct(product, idPrefix) {
-	const titleEl = document.getElementById(`${idPrefix}-product-title`);
-	const priceEl = document.getElementById(`${idPrefix}-product-price`);
-	const imageEl = document.getElementById(`${idPrefix}-product-image`);
+	const titleEl = document.getElementById(`${idPrefix}-title`);
+	const priceEl = document.getElementById(`${idPrefix}-price`);
+	const imageEl = document.getElementById(`${idPrefix}-image`);
   
 	titleEl.textContent = product.title;
 	priceEl.textContent = product.price.toFixed(2) + '€';
@@ -15,7 +20,7 @@ function fetchProduct(id) {
   
   fetchProduct(1)
 	.then((product) => {
-	  updateProduct(product, '1');
+	  updateProduct(product, 'product-1');
 	})
 	.catch((error) => {
 	  console.error('Fehler beim Abrufen des Produkts:', error);
